c49db5d0469ca7bda176410f5f8e55ef2b398808
Andrew Rowberry <rowbs.js@gmail.com>
2022-01-22 13:36:21 -0800

    2 plans

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -6,7 +6,7 @@ const content = {
       credit: 'Jacob Van Blarcom',
       creditLink: 'https://unsplash.com/photos/lkzjENdWgd8',
       message: 'To view this content, you need to create an account!',
-      allowedRoles: ['free', 'pro', 'premium'],
+      allowedRoles: ['free', 'pro'],
     },
     pro: {
       src:
@@ -16,17 +16,7 @@ const content = {
       creditLink: 'https://unsplash.com/photos/yxmNWxi3wCo',
       message:
         'This is protected content! It’s only available if you have a pro plan or higher.',
-      allowedRoles: ['pro', 'premium'],
-    },
-    premium: {
-      src:
-        'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
-      alt: 'corgi in a tent with string lights in the foreground',
-      credit: 'Cole Keister',
-      creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
-      message:
-        'This is protected content! It’s only available if you have the poop plan.',
-      allowedRoles: ['premium'],
+      allowedRoles: ['pro'],
     },
   };
   
