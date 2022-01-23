44cd548dcb1815501140a548765c7450def3037e
Andrew Rowberry <rowbs.js@gmail.com>
2022-01-22 17:12:04 -0800

    config members

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/js');
+  config.addPassthroughCopy('src/members');
   config.addPassthroughCopy('_redirects');
   return {
     dir: {
