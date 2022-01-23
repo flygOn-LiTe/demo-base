0ba2249407e78217bb71e5745e1a056a6b80bd2c
Andrew Rowberry <rowbs.js@gmail.com>
2022-01-22 16:57:06 -0800

    config

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/js');
-
+  config.addPassthroughCopy('_redirects');
   return {
     dir: {
       input: 'src',
