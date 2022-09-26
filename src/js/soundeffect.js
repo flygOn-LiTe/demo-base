a147e89a331c61c19be3c527c57e8197a5f86ca4
Andrew Rowberry <rowbs.js@gmail.com>
2022-09-26 10:04:54 -0700

    Update soundeffect.js

--- src/js/soundeffect.js
+++ src/js/soundeffect.js
@@ -1,17 +1,12 @@
-function isItHidden() {
-  var x = document.getElementById("killBox");
-  if (window.getComputedStyle(x).display === "none") {
-    console.log("it is hidden");
-  } else {
-    console.log("it is showing");
-  }
-}
-isItHidden();
+var soundEffect = new Audio();
+soundEffect.src =
+  "https://uploads.twitchalerts.com/000/140/296/153/superuber.mp3";
 
 var targetNode = document.getElementById("killBox");
 var observer = new MutationObserver(function () {
   if (targetNode.style.display != "none") {
-    console.log("something changed!");
+    console.log("you got a kill!");
+    soundEffect.play();
   }
 });
 observer.observe(targetNode, { attributes: true, childList: true });
