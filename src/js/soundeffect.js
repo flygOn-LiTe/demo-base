e529a752702ac993f3f9d00f0aef02beba0d0e9d
Andrew Rowberry <rowbs.js@gmail.com>
2022-09-26 20:27:12 -0700

    font transition

--- src/js/soundeffect.js
+++ src/js/soundeffect.js
@@ -1,4 +1,5 @@
-var targetNode = document.getElementById("killBox");
+const targetNode = document.getElementById("killBox");
+const playerName = document.querySelector(".playerSlot-player-is-me");
 
 var soundEffect = new Audio();
 soundEffect.src = "https://berrybroscrypto.com/images/witchlaugh.mp3";
@@ -11,6 +12,10 @@ var observer = new MutationObserver(function () {
   if (targetNode.style.display != "none") {
     console.log("you got a kill!");
     soundEffect.play();
+    playerName.setAttribute("font-size", "3em");
+    setTimeout(() => {
+      playerName.setAttribute("font-size", "1em");
+    }, 5000);
   }
 });
 observer.observe(targetNode, { attributes: true, childList: true });
