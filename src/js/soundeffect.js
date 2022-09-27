646d37b261a71310daa65309004c09a9503b3374
Andrew Rowberry <rowbs.js@gmail.com>
2022-09-26 20:51:19 -0700

    Update soundeffect.js

--- src/js/soundeffect.js
+++ src/js/soundeffect.js
@@ -12,9 +12,9 @@ var observer = new MutationObserver(function () {
     console.log("you got a kill!");
     soundEffect.play();
     const playerName = document.querySelector(".playerSlot-player-is-me");
-    playerName.setAttribute("transform", "scale(2);");
+    playerName.setAttribute("style", "transform: scale(2);");
     setTimeout(() => {
-      playerName.setAttribute("transform", "scale(1);");
+      playerName.setAttribute("style", "transform: scale(1);");
     }, 3000);
   }
 });
