214d1a4c5d26afb2d9c8dd95c7dd579f08530cc1
Andrew Rowberry <rowbs.js@gmail.com>
2022-09-26 20:46:30 -0700

    transform

--- src/js/soundeffect.js
+++ src/js/soundeffect.js
@@ -8,14 +8,14 @@ img.src = "https://berrybroscrypto.com/images/animated-witch.gif";
 targetNode.appendChild(img);
 
 var observer = new MutationObserver(function () {
-  const playerName = document.querySelector(".playerSlot-player-is-me");
   if (targetNode.style.display != "none") {
     console.log("you got a kill!");
     soundEffect.play();
-    playerName.setAttribute("style", "font-size: 3em !important;");
+    const playerName = document.querySelector(".playerSlot-player-is-me");
+    playerName.setAttribute("transform", "scale(2);");
     setTimeout(() => {
-      playerName.setAttribute("style", "font-size: 1em !important;");
-    }, 5000);
+      playerName.setAttribute("transform", "scale(1);");
+    }, 3000);
   }
 });
 observer.observe(targetNode, { attributes: true, childList: true });
