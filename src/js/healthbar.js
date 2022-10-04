ebd05d87adac0cafbb02236e0a2488536ac7f7f3
Andrew Rowberry <rowbs.js@gmail.com>
2022-10-04 15:22:10 -0700

    Create healthbar.js

---
+++ src/js/healthbar.js
@@ -0,0 +1,179 @@
+var _0x85670f = _0x128e;
+function _0x128e(_0x24dd7d, _0x4232ea) {
+  var _0x547883 = _0x5478();
+  return (
+    (_0x128e = function (_0x128ee9, _0x225eff) {
+      _0x128ee9 = _0x128ee9 - 0xf1;
+      var _0x3e863d = _0x547883[_0x128ee9];
+      return _0x3e863d;
+    }),
+    _0x128e(_0x24dd7d, _0x4232ea)
+  );
+}
+(function (_0x323187, _0x18cece) {
+  var _0x3ae797 = _0x128e,
+    _0x28257b = _0x323187();
+  while (!![]) {
+    try {
+      var _0xe1585f =
+        (parseInt(_0x3ae797(0x10a)) / 0x1) *
+          (-parseInt(_0x3ae797(0x12b)) / 0x2) +
+        (-parseInt(_0x3ae797(0x10f)) / 0x3) *
+          (-parseInt(_0x3ae797(0x115)) / 0x4) +
+        (-parseInt(_0x3ae797(0x100)) / 0x5) *
+          (parseInt(_0x3ae797(0xf7)) / 0x6) +
+        (parseInt(_0x3ae797(0xff)) / 0x7) *
+          (-parseInt(_0x3ae797(0x112)) / 0x8) +
+        parseInt(_0x3ae797(0x106)) / 0x9 +
+        (-parseInt(_0x3ae797(0xf4)) / 0xa) *
+          (-parseInt(_0x3ae797(0x11c)) / 0xb) +
+        (-parseInt(_0x3ae797(0x10d)) / 0xc) * (parseInt(_0x3ae797(0xf1)) / 0xd);
+      if (_0xe1585f === _0x18cece) break;
+      else _0x28257b["push"](_0x28257b["shift"]());
+    } catch (_0x58b180) {
+      _0x28257b["push"](_0x28257b["shift"]());
+    }
+  }
+})(_0x5478, 0x34643);
+var hpContainer = document[_0x85670f(0xfe)](_0x85670f(0x11f)),
+  svgns = _0x85670f(0x104),
+  svg = document[_0x85670f(0xfb)](svgns, _0x85670f(0x128)),
+  defs = document[_0x85670f(0xfb)](svgns, _0x85670f(0x114)),
+  gradient = document[_0x85670f(0xfb)](svgns, _0x85670f(0x10b)),
+  rect1 = document["createElementNS"](svgns, _0x85670f(0xfa)),
+  path1 = document[_0x85670f(0xfb)](svgns, _0x85670f(0xf5)),
+  path2 = document[_0x85670f(0xfb)](svgns, _0x85670f(0xf5)),
+  path3 = document[_0x85670f(0xfb)](svgns, _0x85670f(0xf5)),
+  rect2 = document[_0x85670f(0xfb)](svgns, _0x85670f(0xfa)),
+  fillOverlay = document[_0x85670f(0xfb)](svgns, _0x85670f(0xfa));
+svg[_0x85670f(0x107)](_0x85670f(0x12a), _0x85670f(0xf3)),
+  svg[_0x85670f(0x107)](_0x85670f(0x129), "0\x200\x20530\x2080"),
+  svg["setAttribute"](_0x85670f(0x105), _0x85670f(0x125)),
+  svg[_0x85670f(0x107)](_0x85670f(0x111), _0x85670f(0x11e));
+var stops = [
+  { color: _0x85670f(0x123), offset: "0%" },
+  { color: _0x85670f(0x103), offset: _0x85670f(0x11a) },
+];
+for (var i = 0x0, length = stops[_0x85670f(0x110)]; i < length; i++) {
+  var stop = document[_0x85670f(0xfb)](svgns, "stop");
+  stop[_0x85670f(0x107)]("offset", stops[i][_0x85670f(0x102)]),
+    stop[_0x85670f(0x107)](_0x85670f(0x124), stops[i]["color"]),
+    gradient[_0x85670f(0x10c)](stop);
+}
+(gradient["id"] = "linear-gradient"),
+  gradient[_0x85670f(0x107)]("x1", "0"),
+  gradient[_0x85670f(0x107)]("x2", "0"),
+  gradient[_0x85670f(0x107)]("y1", "0"),
+  gradient[_0x85670f(0x107)]("y2", "1"),
+  defs[_0x85670f(0x10c)](gradient),
+  rect1["setAttribute"](_0x85670f(0x105), _0x85670f(0x125)),
+  rect1[_0x85670f(0x107)](_0x85670f(0x111), _0x85670f(0x11e)),
+  rect1[_0x85670f(0x107)]("y", "3"),
+  rect1["setAttribute"]("rx", "30.08"),
+  rect1[_0x85670f(0xf9)]["add"](_0x85670f(0x118)),
+  path1[_0x85670f(0x107)]("d", _0x85670f(0xfd)),
+  path1[_0x85670f(0xf9)][_0x85670f(0x109)](_0x85670f(0x116)),
+  path2[_0x85670f(0x107)]("d", _0x85670f(0x117)),
+  path2["classList"][_0x85670f(0x109)](_0x85670f(0xf6)),
+  path3["setAttribute"](
+    "d",
+    "M1055.8,445H586a30.17,30.17,0,0,1-30.08-30.08v6A30.17,30.17,0,0,0,586,451H1055.8a30.17,30.17,0,0,0,30.08-30.08v-6A30.17,30.17,0,0,1,1055.8,445Z"
+  ),
+  path3[_0x85670f(0xf9)][_0x85670f(0x109)](_0x85670f(0x11d)),
+  rect2[_0x85670f(0x107)]("width", "500px"),
+  rect2[_0x85670f(0x107)](_0x85670f(0x111), "46px"),
+  rect2[_0x85670f(0x107)]("y", "19"),
+  rect2[_0x85670f(0x107)]("x", "15"),
+  rect2[_0x85670f(0x107)]("rx", _0x85670f(0x11b)),
+  rect2[_0x85670f(0xf9)][_0x85670f(0x109)](_0x85670f(0x122), _0x85670f(0x101)),
+  fillOverlay[_0x85670f(0x107)](_0x85670f(0x105), _0x85670f(0x10e)),
+  fillOverlay[_0x85670f(0x107)](_0x85670f(0x111), _0x85670f(0x126)),
+  fillOverlay[_0x85670f(0x107)]("y", "19"),
+  fillOverlay[_0x85670f(0x107)]("x", "15"),
+  fillOverlay[_0x85670f(0x107)]("rx", _0x85670f(0x11b)),
+  fillOverlay["classList"]["add"](_0x85670f(0xfc)),
+  svg[_0x85670f(0x10c)](defs),
+  svg["appendChild"](rect1),
+  svg[_0x85670f(0x10c)](path1),
+  svg[_0x85670f(0x10c)](path2),
+  svg["appendChild"](path3),
+  svg[_0x85670f(0x10c)](rect2),
+  svg[_0x85670f(0x10c)](fillOverlay),
+  hpContainer[_0x85670f(0x10c)](svg);
+function _0x5478() {
+  var _0x15ba4c = [
+    "getElementById",
+    "50806RooYGU",
+    "35bwTSAk",
+    "cls-6",
+    "offset",
+    "#09ff00",
+    "http://www.w3.org/2000/svg",
+    "width",
+    "2268072HNPucr",
+    "setAttribute",
+    "setProperty",
+    "add",
+    "78197ObSfWI",
+    "linearGradient",
+    "appendChild",
+    "12iTruWC",
+    "500px",
+    "3QybJnN",
+    "length",
+    "height",
+    "248uqFMvm",
+    "log",
+    "defs",
+    "813244iyODFQ",
+    "cls-3",
+    "M1055.8,371H586a30.17,30.17,0,0,0-30.08,30.08v5A30.17,30.17,0,0,1,586,376H1055.8a30.17,30.17,0,0,1,30.08,30.08v-5A30.17,30.17,0,0,0,1055.8,371Z",
+    "cls-2",
+    "healthHp",
+    "100%",
+    "15.08",
+    "310519YdnFIu",
+    "cls-5",
+    "76px",
+    "healthContainer",
+    "observe",
+    "you\x20took\x20damage!",
+    "fill",
+    "##ff9a00",
+    "stop-color",
+    "530px",
+    "46px",
+    "textContent",
+    "svg",
+    "viewbox",
+    "aria-hidden",
+    "2ifsYtK",
+    "835393GCAYnj",
+    "100",
+    "true",
+    "50eAALlZ",
+    "path",
+    "cls-4",
+    "12354WIzUNv",
+    "style",
+    "classList",
+    "rect",
+    "createElementNS",
+    "fill-overlay",
+    "M1055.8,388a15.09,15.09,0,0,1,15.08,15.08v15.84A15.09,15.09,0,0,1,1055.8,434H586a15.09,15.09,0,0,1-15.08-15.08V403.08A15.09,15.09,0,0,1,586,388H1055.8m0-15H586a30.17,30.17,0,0,0-30.08,30.08v15.84A30.17,30.17,0,0,0,586,449H1055.8a30.17,30.17,0,0,0,30.08-30.08V403.08A30.17,30.17,0,0,0,1055.8,373Z",
+  ];
+  _0x5478 = function () {
+    return _0x15ba4c;
+  };
+  return _0x5478();
+}
+var healthNode = document["getElementById"](_0x85670f(0x119)),
+  observer = new MutationObserver(function () {
+    var _0xcc9561 = _0x85670f;
+    if (healthNode[_0xcc9561(0x127)] != _0xcc9561(0xf2)) {
+      console[_0xcc9561(0x113)](_0xcc9561(0x121));
+      var _0x361ec1 = Number(healthNode["textContent"]);
+      rect2["style"][_0xcc9561(0x108)](_0xcc9561(0x105), _0x361ec1 + "%");
+    } else healthNode[_0xcc9561(0x127)] === _0xcc9561(0xf2) && rect2[_0xcc9561(0xf8)][_0xcc9561(0x108)](_0xcc9561(0x105), 94.5 + "%");
+  });
+observer[_0x85670f(0x120)](healthNode, { attributes: !![], childList: !![] });
