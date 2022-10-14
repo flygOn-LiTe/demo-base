0eb863fdb36e863c96e4f439b9f20f2124e19b91
Andrew Rowberry <rowbs.js@gmail.com>
2022-10-14 07:46:33 -0700

    test

--- src/js/soundeffect.js
+++ src/js/soundeffect.js
@@ -1,5 +1,5 @@
 const targetNode = document.getElementById("killBox");
-
+console.log(vueData);
 var soundEffect = new Audio();
 soundEffect.src = "https://berrybroscrypto.com/images/witchlaugh.mp3";
 
